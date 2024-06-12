452f175d2398cc117e734e18f316259e198fd9e4
anh.phamviet <anh.phamviet@vti.com.vn>
2024-06-12 15:49:15 +0700

    Refactor InternxtClientLoginTests with base class
    The InternxtClientLoginTests was refactored to inherit from the InternxtClientTestBase. The Setup method and redundant code have been removed for cleaner and less repetition. Login method now uses the GetTestLogin function from the base class.

--- InternxtSdk.Tests/InternxtClientTests/InternxtClientLoginTests.cs
+++ InternxtSdk.Tests/InternxtClientTests/InternxtClientLoginTests.cs
@@ -2,28 +2,13 @@ using Newtonsoft.Json;
 
 namespace InternxtSdk.Tests.InternxtClientTests;
 
-public class InternxtClientLoginTests
+public class InternxtClientLoginTests : InternxtClientTestBase
 {
-    private IInternxtClient _client;
-
-    [SetUp]
-    public void Setup()
-    {
-        var userProfilePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-        var nodeJsPath = userProfilePath + "\\AppData\\Roaming\\nvm\\v20.14.0\\node.exe";
-        var internxtCliPath = userProfilePath + "\\AppData\\Roaming\\nvm\\v20.14.0\\node_modules\\@internxt\\cli\\bin\\run.js";
-        _client = new InternxtClient(internxtCliPath, nodeJsPath);
-    }
-
     [Test]
     public async Task Login_CorrectPassword_ReturnTrue()
     {
-        // get username, password from login.json
-        var loginJson = File.ReadAllText("login.json");
-        var loginData = JsonConvert.DeserializeObject<LoginData>(loginJson);
-        var username = loginData.Username;
-        var password = loginData.Password;
-        var result = await _client.LoginAsync(username, password);
+        var loginData = GetTestLogin();
+        var result = await Client.LoginAsync(loginData.Username, loginData.Password);
         Assert.That(result, Is.EqualTo(true));
     }
     
@@ -31,9 +16,9 @@ public class InternxtClientLoginTests
     [Test]
     public async Task Login_IncorrectPassword_ReturnFalse()
     {
-        var username = "testuser";
-        var password = "incorrectpassword";
-        var result = await _client.LoginAsync(username, password);
+        const string username = "testuser";
+        const string password = "incorrectpassword";
+        var result = await Client.LoginAsync(username, password);
         Assert.That(result, Is.EqualTo(false));
     }
 }
