3711f97f67fbffda5eb26785aefab39625fc79a9
anh.phamviet <anh.phamviet@vti.com.vn>
2024-06-14 14:43:45 +0700

    Update InternxtClient tests and settings
    Tests in InternxtClientListTests class were modified to replace hard-coded values with dynamic ones. This change includes creation and usage of a test folder and checking its content more explicitly. Furthermore, Environment settings related to UnitTesting and Highlighting have also been updated to reflect proper test snapshots and paths.

--- InternxtSdk.Tests/InternxtClientTests/InternxtClientListTests.cs
+++ InternxtSdk.Tests/InternxtClientTests/InternxtClientListTests.cs
@@ -3,12 +3,18 @@
 [TestFixture]
 public class InternxtClientListTests : InternxtClientTestBase
 {
+    private string _testFolderId;
+
     public override async Task Setup()
     {
         await base.Setup();
         var loginData = GetTestLogin();
         var result = await Client.LoginAsync(loginData.Username, loginData.Password);
         Assert.That(result, Is.EqualTo(true));
+
+        _testFolderId = await Client.CreateFolderAsync("TestFolder");
+        await Client.CreateFolderAsync("Test Folder", _testFolderId);
+        await Client.UploadAsync("sample\\test.txt", _testFolderId);
     }
 
     [Test]
@@ -16,20 +22,20 @@ public class InternxtClientListTests : InternxtClientTestBase
     {
         var list = await Client.ListAsync();
         Assert.That(list, Is.Not.Null);
-        Assert.That(list.Count, Is.GreaterThan(0));
+        Assert.That(list, Has.Count.GreaterThan(0));
     }
 
     [Test]
     public async Task List_ReturnListOfItemInTestFolder()
     {
-        var list = await Client.ListAsync("498a8bea-56e8-4ac5-83da-5bfef47f19bd");
+        var list = await Client.ListAsync(_testFolderId);
         Assert.That(list, Is.Not.Null);
-        Assert.That(list.Count, Is.EqualTo(2));
+        Assert.That(list, Has.Count.EqualTo(2));
         Assert.Multiple(() =>
         {
             Assert.That(list[0].Name, Is.EqualTo("Test Folder"));
             Assert.That(list[0].Type, Is.EqualTo("folder"));
-            Assert.That(list[1].Name, Is.EqualTo("SYOHIN.zip"));
+            Assert.That(list[1].Name, Is.EqualTo("test.txt"));
             Assert.That(list[1].Type, Is.EqualTo("file"));
         });
     }
