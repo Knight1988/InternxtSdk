f0598ea952fcb4708d7a5a74870ad4267a07a760
anh.phamviet <anh.phamviet@vti.com.vn>
2024-07-24 15:27:18 +0700

    Add console output for login result
    The code has been modified to provide additional visibility for the login operation by writing the result to the console. This modification will help with tracking and debugging login-related issues in the InternxtClient class.

--- DarkKnight.InternxtSdk/InternxtClient.cs
+++ DarkKnight.InternxtSdk/InternxtClient.cs
@@ -18,6 +18,7 @@ public class InternxtClient : IInternxtClient
     public async Task<bool> LoginAsync(string username, string password)
     {
         var result = await ExecuteAsync($"login -n -e {username} -p {password}");
+        Console.WriteLine(result.NormalOutput);
         return result.NormalOutput.Contains($"Succesfully logged in to: {username}");
     }
 
